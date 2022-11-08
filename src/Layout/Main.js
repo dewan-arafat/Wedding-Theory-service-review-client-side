ecb9e7ad04dad758add8f90b4c3947cbd1334f6a
dewan-arafat <dewanarafat1@gmail.com>
2022-11-09 00:51:37 +0600

    first commit

---
+++ src/Layout/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Home from '../Pages/Home';
+import Footer from '../Pages/Shared/Footer';
+import Header from '../Pages/Shared/Header';
+
+const Main = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Home></Home>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
